c1e766e2a11f8abab384dd285f3836f1ffec35e8
Parker <kirbypar@msu.edu>
2020-02-20 11:01:01 -0500

    Added a beginning menu to build from

--- app/src/main/java/edu/msu/team17/chess/ChessActivity.java
+++ app/src/main/java/edu/msu/team17/chess/ChessActivity.java
@@ -3,6 +3,8 @@ package edu.msu.team17.chess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ChessActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ChessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chess_menu, menu);
+        return true;
     }
 }
