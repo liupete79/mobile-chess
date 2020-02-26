a6d46b5b78aa9f25e5bd53f85a3d2b28451538d5
Parker <kirbypar@msu.edu>
2020-02-26 16:29:45 -0500

    Added a new Activity to enter player names

--- app/src/main/java/edu/msu/team17/chess/NameActivity.java
+++ app/src/main/java/edu/msu/team17/chess/NameActivity.java
@@ -1,20 +1,21 @@
 package edu.msu.team17.chess;
 
-import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class NameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_name);
     }
 
-    public void onStartChess(View view) {
-        Intent intent = new Intent(this, NameActivity.class);
+    public void onStartMain(View view) {
+        Intent intent = new Intent(this, ChessActivity.class);
         startActivity(intent);
     }
 }
