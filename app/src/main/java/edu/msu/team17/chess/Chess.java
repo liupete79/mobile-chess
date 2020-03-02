15adf4186ca9a83b6084f86d76f3d23cf23afb23
Peter Liu <liupete1@msu.edu>
2020-03-02 11:49:08 -0500

    Changed Function Name
    Signed-off-by: Peter Liu <liupete1@msu.edu>

--- app/src/main/java/edu/msu/team17/chess/Chess.java
+++ app/src/main/java/edu/msu/team17/chess/Chess.java
@@ -69,11 +69,11 @@ public class Chess {
             return this.piece;
         }
 
-        public void setx(int x) {
+        public void setX(int x) {
             this.x = x;
         }
 
-        public void sety(int y) {
+        public void setY(int y) {
             this.y = y;
         }
 
