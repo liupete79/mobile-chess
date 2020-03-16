d0fc4cd22cce05de8de0aff9463a202721c24446
Parker <kirbypar@msu.edu>
2020-03-16 17:42:50 -0400

    Creates a "isWhite" member variable, allowing to determine between teams

--- app/src/main/java/edu/msu/team17/chess/ChessPieceKnight.java
+++ app/src/main/java/edu/msu/team17/chess/ChessPieceKnight.java
@@ -3,8 +3,8 @@ package edu.msu.team17.chess;
 import android.content.Context;
 
 public class ChessPieceKnight extends ChessPiece {
-    public ChessPieceKnight(Context context, int id, float initialX, float initialY,  int player, int square_id) {
-        super(context, id, initialX, initialY, player, square_id);
+    public ChessPieceKnight(Context context, int id, float initialX, float initialY,  int player, int square_id, boolean white) {
+        super(context, id, initialX, initialY, player, square_id, white);
     }
 
     @Override
