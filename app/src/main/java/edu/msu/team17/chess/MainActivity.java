286dda6af28369b818948e2757eeb8e2c0700579
Parker <kirbypar@msu.edu>
2020-03-30 19:59:43 -0400

    Integrating PHP and Java. Added support for logging into the game.

--- app/src/main/java/edu/msu/team17/chess/MainActivity.java
+++ app/src/main/java/edu/msu/team17/chess/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.msu.team17.chess.Cloud.Cloud;
 
 public class MainActivity extends AppCompatActivity {
+    boolean success = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartChess(View view) {
-        Intent intent = new Intent(this, MatchmakingActivity.class);
-        startActivity(intent);
+        EditText p1 = findViewById(R.id.username);
+        EditText p2 = findViewById(R.id.password);
+        final String username = p1.getText().toString();
+        final String userpassword = p2.getText().toString();
+        new Thread(new Runnable() {
+
+            @Override
+            public synchronized void run() {
+                Cloud cloud = new Cloud();
+                success = cloud.login(username, userpassword);
+            }
+        }).start();
+
+        if(success){
+            Intent intent = new Intent(this, MatchmakingActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(view.getContext(), R.string.logFailed, Toast.LENGTH_SHORT).show();
+        }
     }
     public void onCreateAccount(View view) {
         Intent intent = new Intent(this, CreateAccountActivity.class);
