e39a4c386d152205c755ec992bd321aa025f2592
Peter Liu <liupete1@msu.edu>
2020-04-06 18:18:12 -0400

    Created Cloud Added...stuff.
    Signed-off-by: Peter Liu <liupete1@msu.edu>

--- app/src/main/java/edu/msu/team17/chess/MainActivity.java
+++ app/src/main/java/edu/msu/team17/chess/MainActivity.java
@@ -2,8 +2,11 @@ package edu.msu.team17.chess;
 
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,19 +14,47 @@ import edu.msu.team17.chess.Cloud.Cloud;
 
 public class MainActivity extends AppCompatActivity {
     boolean success = false;
+    boolean saveLogin;
+    private EditText p1;
+    private EditText p2;
+    private CheckBox rememberMe;
+    private SharedPreferences loginPrefs;
+    private SharedPreferences.Editor loginPrefsEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p1 = findViewById(R.id.username);
+        p2 = findViewById(R.id.password);
+        rememberMe = (CheckBox)findViewById(R.id.rememberBox);
+        loginPrefs = getSharedPreferences("logPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPrefs.edit();
+
+        saveLogin = loginPrefs.getBoolean("saveLogin", false);
+        if (saveLogin == true){
+            p1.setText(loginPrefs.getString("username", ""));
+            p2.setText(loginPrefs.getString("userpassword", ""));
+            rememberMe.setChecked(true);
+        }
     }
 
     public void onStartChess(View view) {
-        EditText p1 = findViewById(R.id.username);
-        EditText p2 = findViewById(R.id.password);
+        //EditText p1 = findViewById(R.id.username);
+        //EditText p2 = findViewById(R.id.password);
         final String username = p1.getText().toString();
         final String userpassword = p2.getText().toString();
         final View v = view;
+
+        if (rememberMe.isChecked()) {
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            loginPrefsEditor.putString("username", username);
+            loginPrefsEditor.putString("userpassword", userpassword);
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
+        }
 
         new Thread(new Runnable() {
 
